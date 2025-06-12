eb725d9b749b27bf8217afe86203ff9c41f4008d
alfaniatus <sholehahalfaniatus@gmail.com>
2025-06-12 13:06:35 +0700

    login user

--- app/Http/Controllers/IndikatorController.php
+++ app/Http/Controllers/IndikatorController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Tahun; // ⬅️ pastikan ini ada
 
 class IndikatorController extends Controller
 {
     public function index()
     {
-        return view('admin.indikator'); 
+        $tahunList = Tahun::orderBy('tahun', 'desc')->pluck('tahun');
+        return view('admin.indikator', compact('tahunList')); 
     }
 }
--- resources/views/admin/indikator.blade.php
+++ resources/views/admin/indikator.blade.php
@@ -1,14 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-<div>
-    
+    <div class="bg-white rounded-md px-3 py-4 shadow">
+        <form action="#" method="GET" class="flex justify-end gap-4 items-center">
+            <div>
+                <select name="tahun" class="border px-4 py-2 rounded focus:outline-none">
+                    @foreach ($tahunList as $tahun)
+                        <option value="{{ $tahun }}">{{ $tahun }}</option>
+                    @endforeach
+                </select>
+            </div>
+            <button type="submit" class="bg-[#146082] text-white px-4 py-2 rounded hover:bg-[#104b66] text-sm font-medium">
+                SET
+            </button>
+        </form>
+    </div>
+      <div class="bg-white rounded-md p-6 min-h-[300px] shadow mt-6">
 
-    <div class="bg-white rounded-md p-6 min-h-[300px] shadow">
-        <div class="flex justify-end mb-4">
-            <a href="#" class="bg-[#146082] text-white px-4 py-2 rounded hover:bg-[#104b66] text-sm font-medium">
-                + Tambah Indikator
-            </a>
-        
-</div>
+      </div>
 @endsection
